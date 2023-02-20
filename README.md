076b40c45c4ea505819d0a0734c405455c020ceb
ShuYuHuang <b123767195@gmail.com>
2023-02-20 14:49:55 +0800

    added experiments, produce, readme, requirements

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # video-handel-compress
 Video batch editing for AIA courses clips: add watermark, add title, compress
+
+AIAers, this is your blessing~~
+
+Lets automate the process of your course video:
+
+1. Add watermark to the clip (ffmpeg-python)
+2. Add title page to the front (MoviePy)
+3. Compress and export to mp4 with framerate = 30 fps (ffmpeg-python)
+
+## Installation
+Star, fork and git clone this repo:
+```git clone https://github.com/ShuYuHuang/whisper-subtitle.git```
+
+Install requirements with the following command:
+```pip install -r requirements.txt```
+
+## Usage
+Please proceed your batch production with the folloings:
+1. Start the main program:
+    ```python produce.py```
+2. Enter the file path, e.g. ```data```
+```
+Please have the folder organized like this
+root-
+    - XXX.mkv(/.mov/.mp4/..., the recorded clip)
+    - XXX.png (the corresponding title picture, please follow the template we made)
+    - watermark.png (the watermark file to print on each frame)
+```
+3. Enter the output path, e.g. ```out```
+4. Wait for 10 minutes for each file
+5. Get your files in the output path and put that to youtube
+6. You can further transcribe the films by [Whisper-Subtitle](https://github.com/ShuYuHuang/whisper-subtitle)
+
+All the tryout notebooks are in ```experiments```
+
+## References
+* PyMovie: https://zulko.github.io/moviepy/getting_started/videoclips.html
+* FFMPEG-python: https://github.com/kkroening/ffmpeg-python
+* FFMPEG fade-in: https://dev.to/dak425/add-fade-in-and-fade-out-effects-with-ffmpeg-2bj7#:~:text=Using%20ffmpeg%2C%20you%20can%20apply,to%20achieve%20a%20similar%20effect.&text=This%20will%20start%20the%20video,full%20view%20over%203%20seconds.
+* https://github.com/kkroening/ffmpeg-python/blob/master/examples/README.md
+* https://dev.to/dak425/add-fade-in-and-fade-out-effects-with-ffmpeg-2bj7
---
+++ produce.py
@@ -0,0 +1,82 @@
+import os
+import re
+from glob import glob
+
+import moviepy.editor as mpy  # for title clip
+import ffmpeg  # watermark, concatenate (w/ for fade-in, out), produce output
+
+SUFFIX_LIST = ['.mkv', '.mp4', '.mov']
+
+if __name__ == '__main__':
+    tmp = input("Type video directory:")
+    if tmp == '':
+        DATA_DIR = '/project/ta-shared-ii/video-handel-compress/data'
+    else:
+        DATA_DIR = tmp
+
+    tmp = input("Type output directory:")
+    if tmp == '':
+        OUTPUT_DIR = '/project/ta-shared-ii/video-handel-compress/output'
+    else:
+        OUTPUT_DIR = tmp
+
+    print('-----Create temperary folder and output folder')
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    os.makedirs('tmp',exist_ok=True)
+    TMP_DIR = 'tmp'
+
+    print('-----List all files from DATA_DIR with suffix in SUFFIX_LIST, find their title picture in ".png" format')
+    SUFFIX_LIST = ['.mkv', '.mp4', '.mov']
+    DATA_DIR = '/project/ta-shared-ii/video-handel-compress/data'
+    all_files = glob(DATA_DIR+'/*.*')
+    vfiles = []
+    for f in all_files:
+        if re.findall(fr'{"|".join(SUFFIX_LIST)}',f):
+            vfiles.append(f)
+    tfiles = list(
+        map(
+            lambda x: re.sub(f'{"|".join(SUFFIX_LIST)}', '.png', x),
+            vfiles
+        )
+    )
+
+    chapters = list(
+        map(
+            lambda x: x.split('/')[-1][:-4],
+            vfiles
+        )
+    )
+
+    print('Create title films')
+    # Prepare dummy audio file
+    make_frame = lambda t: 0.0
+    dummy_audio = mpy.AudioClip(make_frame, duration=3.5, fps=44100) 
+    # Start making title films
+    tmp_filename = []
+    for f, ch in zip(tfiles, chapters):
+        # Title clip
+        tmp_filename.append(f'{TMP_DIR}/{ch}_title.mkv')
+        if not os.path.exists(tmp_filename[-1]):
+            tclip = mpy.ImageClip(f, duration=3.5)
+            tclip = tclip.set_audio(dummy_audio)
+            tclip.write_videofile(tmp_filename[-1], codec='libx264', fps=30, bitrate='2500K')
+
+    print('Producing start:')
+    for tfanme, vfname, ch in zip(tmp_filename, vfiles, chapters):
+        # Set input files
+        t_in = ffmpeg.input(tfanme)
+        v_in = ffmpeg.input(vfname)
+        wm_in = ffmpeg.input(DATA_DIR+'/watermark.png')
+
+        # Add water mark to main video
+        fused = ffmpeg.overlay(v_in.video, wm_in)
+
+        # Add title clip
+        joined = ffmpeg.concat(t_in.video.filter_('fade', t='out', st=3.0, d=0.5),
+                               t_in.audio,
+                               fused.filter_('fade', t='in', st=0, d=0.5),
+                               v_in.audio,
+                               v=1, a=1)
+        # Run Production
+        out = ffmpeg.output(joined, f'{OUTPUT_DIR}/{ch}.mp4',r=30)
+        out.run()
